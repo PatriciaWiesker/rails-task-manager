78c9962d25516673b7301f7a6edd6230be55775e
Patricia Wiesker <patricia@wiesker.de>
2017-03-14 18:57:49 +0800

    Day2 rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
+  resources :tasks
+
+  # # Index
+  # get 'tasks/', to: "tasks#index"
+
+  # # New = New + Create
+  # # New
+  # get 'tasks/new', to: "tasks#new"
+  # # Create
+  # post 'tasks/', to: "tasks#create"
+
+  # # Show
+  # get 'tasks/:id', to: "tasks#show"
+
+  # # Edit = Edit + Update
+  # # Edit
+  # get 'tasks/:id/edit', to: "tasks#edit"
+  # # Update
+  # patch 'tasks/:id', to: "tasks#update", as: "update_task"
+
+  # # Destroy
+  # delete 'tasks/:id', to: "tasks#destroy", as: "destroy_task"
+
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
